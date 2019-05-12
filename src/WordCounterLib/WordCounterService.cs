c730b45ef7560b8d8a98e5423a182e823e9d15ee
Rapzy <andrushko.egor@gmail.com>
2019-05-12 14:58:21 +0300

    Create Service

---
+++ src/WordCounterLib/WordCounterService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WordCounterLib
+{
+    public class WordCounterService : IWordCounter
+    {
+        public int CountWords(string text)
+        {
+            int index = 0, wordCount = 0;
+            while (index < text.Length && char.IsWhiteSpace(text[index]))
+                index++;
+            while (index < text.Length)
+            {
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+                wordCount++;
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+            return wordCount;
+        }
+    }
+}
