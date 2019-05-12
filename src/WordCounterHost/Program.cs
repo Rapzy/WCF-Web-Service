7d1cbd69b1cdfeaf556687182fad47c4eea61b89
Rapzy <andrushko.egor@gmail.com>
2019-05-12 14:58:43 +0300

    Create Host

---
+++ src/WordCounterHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WordCounterLib;
+
+namespace WordCounterHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/WordCounterLib/");
+
+            // Step 2: Create a ServiceHost instance.
+            ServiceHost selfHost = new ServiceHost(typeof(WordCounterService), baseAddress);
+
+            try
+            {
+                // Step 3: Add a service endpoint.
+                selfHost.AddServiceEndpoint(typeof(IWordCounter), new WSHttpBinding(), "WordCounterService");
+
+                // Step 4: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5: Start the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                // Close the ServiceHost to stop the service.
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
